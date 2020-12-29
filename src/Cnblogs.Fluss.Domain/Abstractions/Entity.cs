d16a66a99e4f5ae38309bf25d65e8fe387677d35
沈星繁 <ikesnowy@outlook.com>
2020-12-29 14:27:25 +0800

    feat: add domain abstractions

---
+++ src/Cnblogs.Fluss.Domain/Abstractions/Entity.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+
+namespace Cnblogs.Fluss.Domain.Abstractions
+{
+    public abstract class Entity<TKey>
+    {
+        public TKey Id { get; set; }
+
+        private readonly List<IDomainEvent> _events = new();
+
+        protected Entity(TKey id)
+        {
+            Id = id;
+        }
+
+        public IReadOnlyCollection<IDomainEvent> DomainEvents => _events.AsReadOnly();
+
+        public void AddDomainEvent(IDomainEvent eventItem)
+        {
+            _events.Add(eventItem);
+        }
+
+        public void RemoveDomainEvent(IDomainEvent eventItem)
+        {
+            _events.Remove(eventItem);
+        }
+
+        public void ClearDomainEvents()
+        {
+            _events.Clear();
+        }
+
+        public virtual bool IsTransient()
+        {
+            return EqualityComparer<TKey>.Default.Equals(Id, default);
+        }
+    }
+}
---
+++ src/Cnblogs.Fluss.Domain/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cnblogs.Fluss.Domain.Abstractions
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+        Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default);
+        Task RollbackAsync(CancellationToken cancellationToken = default);
+    }
+}
