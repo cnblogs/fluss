62f86e2149ea7c113ca0e0674650c817ac42c67c
沈星繁 <ikesnowy@outlook.com>
2021-01-11 10:57:04 +0800

    feat: add unit tests and functional tests

---
+++ test/Cnblogs.Fluss.FunctionalTests/FlussWebApplicationFactory.cs
@@ -0,0 +1,10 @@
+using Cnblogs.Fluss.Web;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Cnblogs.Fluss.FunctionalTests
+{
+    public class FlussWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+        
+    }
+}
---
+++ test/Cnblogs.Fluss.UnitTests/Infrastructure/QueryExtensionTests.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Linq;
+using Cnblogs.Fluss.Infrastructure;
+using FluentAssertions;
+using Xunit;
+
+namespace Cnblogs.Fluss.UnitTests.Infrastructure
+{
+    public class QueryExtensionTests
+    {
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public void PageIndex_NegativeOrZero_Throw(int pageIndex)
+        {
+            // Arrange
+            var queryable = GetTestQueryable();
+            
+            // Act
+            Action act = () => queryable.Page(pageIndex, 10);
+            
+            // Assert
+            act.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public void PageSize_NegativeOrZero_Throw(int pageSize)
+        {
+            // Arrange
+            var queryable = GetTestQueryable();
+            
+            // Act
+            Action act = () => queryable.Page(1, pageSize);
+            
+            // Assert
+            act.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 3)]
+        [InlineData(1000, 1000)]
+        public void NormalCase_GetPagedData(int pageIndex, int pageSize)
+        {
+            // Arrange
+            var queryable = GetTestQueryable();
+            
+            // Act
+            var result = queryable.Page(pageIndex, pageSize).ToList();
+            
+            // Assert
+            result.Should().BeEquivalentTo(queryable.Skip((pageIndex - 1) * pageSize).Take(pageSize));
+        }
+
+        private static IQueryable<int> GetTestQueryable() => Enumerable.Range(0, 100).AsQueryable();
+    }
+}
