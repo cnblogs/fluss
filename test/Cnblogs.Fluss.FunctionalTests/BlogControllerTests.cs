b430dd15b43674e4de43dde07af5e4f0109a17e0
沈星繁 <ikesnowy@outlook.com>
2021-01-11 11:06:57 +0800

    feat: add unit tests and functional tests (#18)
    * feat: add unit tests and functional tests
    
    * style: remove redundant empty line

---
+++ test/Cnblogs.Fluss.FunctionalTests/BlogControllerTests.cs
@@ -0,0 +1,46 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Cnblogs.Fluss.FunctionalTests
+{
+    public class BlogControllerTests : IClassFixture<FlussWebApplicationFactory>
+    {
+        private readonly FlussWebApplicationFactory _fixture;
+
+        public BlogControllerTests(FlussWebApplicationFactory fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task Home_GetBlogPostList()
+        {
+            // Arrange
+            var client = _fixture.CreateClient();
+            
+            // Act
+            var response = await client.GetAsync("/");
+            var content = await response.Content.ReadAsStringAsync();
+            
+            // Assert
+            response.IsSuccessStatusCode.Should().BeTrue();
+            content.Should().NotBeNullOrEmpty();
+        }
+
+        [Fact]
+        public async Task PostDetail_GetPostDetail()
+        {
+            // Arrange
+            var client = _fixture.CreateClient();
+            
+            // Act
+            var response = await client.GetAsync("/p/1.html");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.IsSuccessStatusCode.Should().BeTrue();
+            content.Should().NotBeNullOrEmpty();
+        }
+    }
+}
---
+++ test/Cnblogs.Fluss.FunctionalTests/FlussWebApplicationFactory.cs
@@ -0,0 +1,9 @@
+using Cnblogs.Fluss.Web;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Cnblogs.Fluss.FunctionalTests
+{
+    public class FlussWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+    }
+}
